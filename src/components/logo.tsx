19e4bfa97aa6335112671f1d12b1fa2a0200ef64
Матвей Стасевич <yngmatteo58@gmail.com>
2025-10-05 18:16:50 +0000

    studio-9145918900:~/studio{main}$ git commit -m "/" [main 56721c8] /  3

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export function Logo(props: Omit<React.ComponentProps<typeof Image>, 'src' | 'alt'>) {
   return (
     <Image
-      src="/images/logo.jpg"
+      src="/images/logo.png"
       alt="CyberStrike Logo"
       width={48}
       height={48}
